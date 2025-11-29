25ee2019b672b3f2378407bc968d798542be8213
nutkat <nutkat7760@gmail.com>
2025-11-29 07:29:05 +0700

    Update Program.cs
    Changes from GIt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! First Change");
+Console.WriteLine("Hello, World! First Change Again I achanged the Data from GIT");
